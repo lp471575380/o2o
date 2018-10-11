dd1f44d87d119b86db43883e9bcf464f99c39b59
lee steven <1836978833@qq.com>
2018-10-11 17:29:25 +0800

    提交主后台模块

---
+++ application/admin/controller/Category.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Category extends Controller
+{
+	public function add()
+	{
+		return $this->fetch();
+	}
+	public function index()
+	{
+		return $this->fetch();
+	}
+	public function save()
+	{
+		//
+	}
+}
